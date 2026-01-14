64a288c7dcdd10d2b2cf1d0830df5f25eb832671
drishkc <kcdrish32@gmail.com>
2026-01-14 13:27:41 +0545

    first commit

---
+++ ex1/jsex.js
@@ -0,0 +1,23 @@
+const button = document.querySelector('#btn');
+const container = document.querySelector('#container');
+const heading1 = document.querySelector('#heading1');
+console.log(3);
+let isToggled = false;
+
+container.addEventListener('click', () => {
+
+    if(!isToggled){
+        heading1.style.color = 'black';
+        container.style.background = 'white';
+        document.body.style.background = 'black';
+        container.style.boxShadow = "0 2px 12px rgb(255, 255, 255)";
+    }
+    else {
+        heading1.style.color = 'white';
+        container.style.background = 'black';
+        document.body.style.background = 'white';
+        container.style.boxShadow = "0 3px 12px rgba(0, 0, 0, 0.5)";
+    }
+    isToggled = !isToggled;
+});
+
